cde683c360d222dfa45f9d6fb13578e9a3dc0cd5
Reese Stichter <rsticht@bu.edu>
2026-02-23 18:52:26 -0500

    Did a lot to try and have the backend and frontend running on Vercel/Railway

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from supabase_client import supabase
 
 app = FastAPI()
+
+# ✅ ADD THIS
+origins = [
+    "http://localhost:5173",
+    "https://clock-in-orcin.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
