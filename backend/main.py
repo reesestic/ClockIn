3977c8648f44552a1a4cc5d4477d1cd112d2c896
Kevin Kupeli <kkupeli@bu.edu>
2026-03-05 18:50:53 -0500

    commented out fast api controller connections since controller is not built yet

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from controllers import sticky_note_controller
 
-app = FastAPI()
+# app = FastAPI()
 
-app.include_router(sticky_note_controller.router)
+# app.include_router(sticky_note_controller.router)
 
 
 # Only allows FASTAPI to take requests from these frontends (security)
@@ -14,10 +14,10 @@ origins = [
     "https://clock-in-orcin.vercel.app"
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
