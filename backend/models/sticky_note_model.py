b9ab57166682e6ab1064f801d127b536c94d4d34
Reese Stichter <rsticht@bu.edu>
2026-03-07 20:32:34 -0500

    just changed order of position and stickyNoteCreate since python got all mad at me or whatever. please pass ci/cd now

--- backend/models/sticky_note_model.py
+++ backend/models/sticky_note_model.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
 
+class Position(BaseModel):
+    x: int
+    y: int
+    z: int
+
 class StickyNoteCreate(BaseModel):
     title: str
     content: str
     position: Position
 
-class Position(BaseModel):
-    x: int
-    y: int
-    z: int
