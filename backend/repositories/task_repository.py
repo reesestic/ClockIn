7b1ac65fb87ffee072fbfdd2d95275248df0fb0e
Kevin Kupeli <kkupeli@bu.edu>
2026-03-10 21:29:22 -0400

    Added a comment to show how the create method works and redeploy CI/CD

--- backend/repositories/task_repository.py
+++ backend/repositories/task_repository.py
@@ -3,4 +3,5 @@ class TaskRepository:
         self.supabase = supabase
 
     def create(self, task_data):  
+        #given the task data from the service, send it to the database!
         response = self.supabase.table('Tasks').insert(task_data).execute()
