ca3bb83dd5561b22d49567f7ae844e545a2653fe
Kevin Kupeli <kkupeli@bu.edu>
2026-04-27 00:39:57 -0400

    removed .single() from repository call and also added RLS policy in Supabase

--- backend/repositories/user_visits_repository.py
+++ backend/repositories/user_visits_repository.py
@@ -7,7 +7,6 @@ class UserVisitsRepository:
         response = supabase.table("user_visits") \
             .select("visited_home, visited_notes, visited_tasks, visited_schedule, visited_timer, visited_garden") \
             .eq("id", user_id) \
-            .single() \
             .execute()
         print("supabase response:", response.data)
         return response.data
